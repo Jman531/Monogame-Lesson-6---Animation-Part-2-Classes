7eb9fee3302fe499affa2df0d88a7a34542994a7
Jack Gallagher <339690067@GoTVDSB.ca>
2024-11-12 09:39:58 -0500

    Add project files.

---
+++ Monogame Lesson 6 - Animation Part 2 Classes/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame_Lesson_6___Animation_Part_2_Classes.Game1();
+game.Run();
---
+++ Monogame Lesson 6 - Animation Part 2 Classes/Tribble.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+
+namespace Monogame_Lesson_6___Animation_Part_2_Classes
+{
+    internal class Tribble
+    {
+        private Texture2D _texture;
+        private Rectangle _rectangle;
+        private Vector2 _speed;
+        
+        public Tribble(Texture2D texture, Rectangle rect, Vector2 speed)
+        {
+            _texture = texture;
+            _rectangle = rect;
+            _speed = speed;
+        }
+
+        public Texture2D Texture
+        {
+            get { return _texture; }
+        }
+
+        public Rectangle Bounds
+        {
+            get { return _rectangle; }
+            set { _rectangle = value; }
+        }
+
+        public void Move(Rectangle window)
+        {
+            _rectangle.Offset(_speed);
+
+            if (_rectangle.Right > window.Width || _rectangle.Left < 0)
+                _speed.X *= -1;
+            if (_rectangle.Bottom > window.Height || _rectangle.Top < 0)
+                _speed.Y *= -1;
+        }
+    }
+}
